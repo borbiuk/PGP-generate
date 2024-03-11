2245ddfa0d80dbc3ba0e8dcdfe3ade02c6090b60
Nazariy Borbiuk <borbiuk.n@gmail.com>
2024-03-11 22:13:12 +0200

    Use js and openpgp

--- index.js
+++ index.js
@@ -1,114 +1,96 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = require("fs");
-var crypto_1 = require("crypto");
-var util_1 = require("util");
-var path_1 = require("path");
-// Generate PGP key pair and optionally print or save to files
-var generatePGPKeyPair = function () {
-    return (0, util_1.promisify)(crypto_1.generateKeyPair)('rsa', {
-        modulusLength: 2048,
-        publicKeyEncoding: { type: 'spki', format: 'pem' },
-        privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
-    });
-};
-var printKeys = function (_a) {
-    var privateKey = _a.privateKey, publicKey = _a.publicKey;
-    console.log('Private Key:', privateKey);
-    console.log('Public Key:', publicKey);
-};
-var saveKeysToFile = function (_a, fileName) {
-    var privateKey = _a.privateKey, publicKey = _a.publicKey;
-    return __awaiter(void 0, void 0, void 0, function () {
-        var privateKeyPath, publicKeyPath;
-        return __generator(this, function (_b) {
-            switch (_b.label) {
-                case 0:
-                    privateKeyPath = (0, path_1.join)(__dirname, fileName + '.private');
-                    publicKeyPath = (0, path_1.join)(__dirname, fileName + '.public');
-                    // Ensure that the directories in the path exist
-                    return [4 /*yield*/, fs_1.promises.mkdir((0, path_1.dirname)(privateKeyPath), { recursive: true })];
-                case 1:
-                    // Ensure that the directories in the path exist
-                    _b.sent();
-                    // Write private and public keys to files
-                    return [4 /*yield*/, fs_1.promises.writeFile(privateKeyPath, privateKey)];
-                case 2:
-                    // Write private and public keys to files
-                    _b.sent();
-                    return [4 /*yield*/, fs_1.promises.writeFile(publicKeyPath, publicKey)];
-                case 3:
-                    _b.sent();
-                    console.log("Key pair files generated: ".concat(privateKeyPath, " and ").concat(publicKeyPath));
-                    return [2 /*return*/];
-            }
-        });
-    });
-};
-var main = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var args, fileNameIndex, printFlagIndex, fileName, print, keys;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                args = process.argv.slice(2);
-                fileNameIndex = args.indexOf('--fileName');
-                printFlagIndex = args.indexOf('--print');
-                if (fileNameIndex === -1) {
-                    console.error('Please provide a fileName parameter using --fileName.');
-                    return [2 /*return*/];
-                }
-                fileName = args[fileNameIndex + 1];
-                print = printFlagIndex !== -1;
-                return [4 /*yield*/, generatePGPKeyPair()];
-            case 1:
-                keys = _a.sent();
-                if (!print) return [3 /*break*/, 2];
-                printKeys(keys);
-                return [3 /*break*/, 4];
-            case 2: return [4 /*yield*/, saveKeysToFile(keys, fileName)];
-            case 3:
-                _a.sent();
-                _a.label = 4;
-            case 4: return [2 /*return*/];
-        }
-    });
-}); };
-main().catch(function (error) {
-    console.error('Error generating PGP key pair:', error);
-});
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import * as openpgp from 'openpgp';
+
+
+const keySizes = [1024, 2048, 4096, 8194];
+
+// Generate PGP key pair and optionally print or save to files.
+const generatePGPKeyPair = async (name, email, keySize, passphrase) => {
+	return await openpgp.generateKey({
+		type: 'rsa',
+		rsaBits: keySize,
+		userIDs: [{ name , email }],
+		passphrase: passphrase,
+	});
+}
+
+// Print keys to console.
+const printKeys = ({ privateKey, publicKey }) => {
+	console.log('Private Key:', privateKey);
+	console.log('Public Key:', publicKey);
+}
+
+// Write keys to .private and .public files.
+const saveKeysToFile = async ({ privateKey, publicKey }, fileName) => {
+	// Set file paths for private and public keys
+	const privateKeyPath = join(__dirname, fileName + '.private');
+	const publicKeyPath = join(__dirname, fileName + '.public');
+
+	// Ensure that the directories in the path exist
+	await fs.mkdir(dirname(privateKeyPath), { recursive: true });
+
+	// Write private and public keys to files
+	await fs.writeFile(privateKeyPath, privateKey);
+	await fs.writeFile(publicKeyPath, publicKey);
+
+	console.log(`Key pair files generated: ${privateKeyPath} and ${publicKeyPath}`);
+}
+
+const main = async () => {
+
+	// Process command line arguments
+	const args = process.argv.slice(2);
+	const nameIndex = args.indexOf('--name');
+	const emailIndex = args.indexOf('--email');
+	const levelIndex = args.indexOf('--level');
+	const passphraseIndex = args.indexOf('--passphrase');
+
+	const fileNameIndex = args.indexOf('--fileName');
+	const printFlagIndex = args.indexOf('--print');
+
+	if (nameIndex === -1 || emailIndex === -1 || passphraseIndex === -1 || (fileNameIndex === -1 && printFlagIndex === -1)) {
+		console.error('Invalid parameters.');
+		return;
+	}
+
+	// Extract fileName and print flag from command line arguments
+	const name = args[nameIndex + 1];
+	const email = args[emailIndex + 1];
+	const passphrase = args[passphraseIndex + 1];
+
+	const levelParam = process.argv[levelIndex + 1];
+	let keySize;
+	if (levelParam !== undefined) {
+		const level = parseInt(levelParam, 10);
+
+		if (!isNaN(level) && level >= 0 && level < keySizes.length) {
+			keySize = keySizes[level];
+		} else {
+			keySize = 4096;
+		}
+	} else {
+		keySize = 4096;
+	}
+
+	const fileName = args[fileNameIndex + 1];
+	const print = printFlagIndex !== -1;
+
+	// Generate and handle PGP key pair
+	const keys = await generatePGPKeyPair(name, email, keySize, passphrase);
+	if (print) {
+		printKeys(keys);
+	}
+	else {
+		await saveKeysToFile(keys, fileName);
+	}
+}
+
+main().catch(error => {
+	console.error('Error generating PGP key pair:', error);
+})
